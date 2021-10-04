7e42693ec24b0a81f6e6584b6dc09c68097014fc
rickutino <rickutino@gmail.com>
2021-10-04 23:12:31 +0900

    finalizaded exame

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,29 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOneOrFail({
+      where: { id: user_id },
+      relations: ["games"],
+    })
+
+    return user as User;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return this.repository.query(`
+      SELECT * FROM users ORDER BY first_name ASC;
+    `);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return this.repository.query(`
+      SELECT first_name, last_name, email FROM users
+      WHERE lower(first_name)=lower('${first_name}') AND lower(last_name)=lower('${last_name}');
+    `);
   }
 }
